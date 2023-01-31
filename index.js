a5cc78e3018eca6b26c87832485f515307ba493d
codedit334 <oussama7elabdioui@gmail.com>
2023-01-31 16:16:21 +0100

    Make module appear and disappear using JS

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 const hamburger = document.querySelector('.icon-Menu');
 const x = document.querySelector('.menu-17');
 const navMenu = document.querySelector('.menu-p');
@@ -11,3 +12,110 @@ x.addEventListener('click', () => {
 document.querySelectorAll('.menu-r').forEach((n) => n.addEventListener('click', () => {
   navMenu.classList.toggle('active');
 }));
+
+// Displaying modals
+
+const modalsObj = [
+  {
+    modalTitle: 'Keeping track of hundreds  of components website',
+    modalList: ['css', 'html', 'Bootstrap', 'Ruby on rails'],
+    modalImg: './images/Snapshoot Portfolio.png',
+    modalText:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. This has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+  },
+  {
+    modalTitle: 'Data Dashboard Healthcare',
+    modalList: ['html', 'Bootstrap', 'Ruby'],
+    modalImg: './images/Snapshoot Portfolio.png',
+    modalText:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+  },
+  {
+    modalTitle: 'Data Dashboard Healthcare',
+    modalList: ['html', 'Bootstrap', 'Ruby'],
+    modalImg: './images/Snapshoot Portfolio.png',
+    modalText:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+  },
+  {
+    modalTitle: 'Website Protfolio',
+    modalList: ['html', 'Bootstrap', 'Ruby'],
+    modalImg: './images/Snapshoot Portfolio.png',
+    modalText:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+  },
+  {
+    modalTitle: 'Profesional Art Printing Data More',
+    modalList: ['html', 'Bootstrap', 'Ruby'],
+    modalImg: './images/Snapshoot Portfolio.png',
+    modalText:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+  },
+  {
+    modalTitle: 'Data Dashboard Healthcare',
+    modalList: ['html', 'Bootstrap', 'Ruby'],
+    modalImg: './images/Snapshoot Portfolio.png',
+    modalText:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+  },
+  {
+    modalTitle: 'Website Protfolio',
+    modalList: ['html', 'Bootstrap', 'Ruby'],
+    modalImg: './images/Snapshoot Portfolio.png',
+    modalText:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+  },
+];
+
+const modals = document.querySelector('.pop-upD');
+
+let modalCount = 1;
+
+modalsObj.forEach((o) => {
+
+  let techStack = '';
+
+  for (let i = 0; i < o.modalList.length; i += 1) techStack += `<li>${o.modalList[i]}</li>`;
+
+  modals.innerHTML += `
+  
+  <div style="z-index=19000" class="modal-${modalCount}">
+      <div class="h-popD">
+          <h3>${o.modalTitle}</h3>
+          <img style="z-index:20000" onClick="closeModal(${modalCount});" src="./images/Icon17.png" alt="">
+      </div>
+      <ul>
+           ${techStack}
+      </ul>
+      <div class="details-popD">
+          <img src="${o.modalImg}" alt="">
+          <div class="pop-B">
+              <p>${o.modalText}</p>
+              <div class="btn-popD">
+                  <button type="button" class="btn-pop btn-pop2">See Live <img src="./images/Icon - Export.png"
+                          alt=""></button>
+                  <button type="button" class="btn-pop btn-pop3">See Source <img src="./images/Vectorpop.png"
+                          alt=""></button>
+              </div>
+          </div>
+      </div>
+  </div>
+`;
+  modalCount += 1;
+});
+
+// Show Modal
+const showModal = (i) => {
+  modals.classList.toggle('active-modal');
+  document.querySelector('.pop-BG').style.display = 'block';
+  document.querySelector(`.modal-${i}`).style.display = 'block';
+};
+
+// Close Modal
+function closeModal(i) {
+  modals.classList.toggle('active-modal');
+  document.querySelector('.pop-BG').style.display = 'none';
+  document.querySelector(`.modal-${i}`).style.display = 'none';
+}
+
+/* eslint-enable no-unused-vars */
