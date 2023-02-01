1ec08202c7599608b3d49f981d2dc3ab48748a30
ikoote1 <ikootepreim@gmail.com>
2023-02-02 00:41:26 +0300

    Coded my work section in javascript and  fixed pop-up  styling

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const modalsObj = [
       "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
   },
   {
-    modalTitle: 'Data Dashboard Healthcare',
+    modalTitle: 'Profesional Art Printing Data More',
     modalList: ['html', 'Bootstrap', 'Ruby'],
     modalImg: './images/Snapshoot Portfolio.png',
     modalText:
@@ -45,14 +45,14 @@ const modalsObj = [
       "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
   },
   {
-    modalTitle: 'Profesional Art Printing Data More',
+    modalTitle: 'Data Dashboard Healthcare',
     modalList: ['html', 'Bootstrap', 'Ruby'],
     modalImg: './images/Snapshoot Portfolio.png',
     modalText:
       "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
   },
   {
-    modalTitle: 'Data Dashboard Healthcare',
+    modalTitle: 'Profesional Art Printing Data More',
     modalList: ['html', 'Bootstrap', 'Ruby'],
     modalImg: './images/Snapshoot Portfolio.png',
     modalText:
@@ -119,4 +119,117 @@ function closeModal(i) {
   document.querySelector(`.modal-${i}`).style.display = 'none';
 }
 
-/* eslint-enable no-unused-vars */
+// work section 1.
+
+const modalsOb = [
+  {
+    modalTitl: 'Components website',
+    modalLis: ['css', 'html', 'Bootstrap', 'Ruby'],
+    modalIm: './images/Img Placeholder.png',
+    modalTex:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. This has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+  }];
+const work = document.querySelector('.description-med');
+
+modalsOb.forEach((o) => {
+  let techStack = '';
+
+  for (let i = 0; i < o.modalLis.length; i += 1) techStack += `<li>${o.modalLis[i]}</li>`;
+
+  work.innerHTML = `
+  <div class="portfolio" id="portfolio">
+  <div>
+  <h2 class="myrr" id="myrr">My Recent Work</h2>
+  <img src="./images/Vecto1.png" alt="blackline" class="blackline">
+</div>
+<div class="img-pp"> <img src="${o.modalIm}"></div>
+<div class="description" id="description">
+  <h2 class="multi">${o.modalTitl}</h2>
+  <p class="daily">${o.modalTex}</p>
+  <ul class="languages">
+          ${techStack} </ul>
+  <button type="button" onclick="showModal(1);" class="but1">See project</button>
+</div>
+<div class="title-med">
+  <div class="img-med"><img src="./images/Img Placeholder1.png"></div>
+  <div class="img-medm"><img src="${o.modalIm}"></div>
+  <div class="title-tree">
+      <h2 class="multi">${o.modalTitl}</h2>
+      <p class="daily">${o.modalTex}</p>
+      <ul class="languages">
+      ${techStack}
+      </ul>
+      <button type="button" onclick="showModal(1);" class="but2">See project</button>
+  </div>
+</div>     
+</div>
+ 
+
+`;
+});
+
+// WORK SECTION 2
+const modalsO = [
+
+  {
+    modalTitle: 'Data Dashboard Healthcare',
+    modalList: ['html', 'Bootstrap', 'Ruby'],
+    modalText:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+  },
+  {
+    modalTitle: 'Profesional Art Printing Data More',
+    modalList: ['html', 'Bootstrap', 'Ruby'],
+    modalText:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+  },
+  {
+    modalTitle: 'Website Protfolio',
+    modalList: ['html', 'Bootstrap', 'Ruby'],
+    modalText:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+  },
+  {
+    modalTitle: 'Profesional Art Printing Data More',
+    modalList: ['html', 'Bootstrap', 'Ruby'],
+    modalText:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+  },
+  {
+    modalTitle: 'Data Dashboard Healthcare',
+    modalList: ['html', 'Bootstrap', 'Ruby'],
+    modalText:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+  },
+  {
+    modalTitle: 'Website Protfolio',
+    modalList: ['html', 'Bootstrap', 'Ruby'],
+    modalText:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+  },
+];
+
+const modalT = document.querySelector('.frams');
+
+let modalCountT = 1;
+
+modalsO.forEach((o) => {
+  let techStack = '';
+
+  for (let i = 0; i < o.modalList.length; i += 1) techStack += `<li>${o.modalList[i]}</li>`;
+
+  modalT.innerHTML += `
+  <div class="grid-item3 modalT-${modalCountT}">
+    <div class="grid-medd3">
+        <h2 class="proff">${o.modalTitle}</h2>
+        <p class="daily1">${o.modalText}</p>
+        <ul class="languagess">
+        ${techStack}
+        </ul>
+        <button type="button" onclick="showModal(${modalCountT});" class="but">See project</button>
+    </div>
+    <button type="button" onclick="showModal(${modalCountT});" class="but3">See project</button>
+</div>
+`;
+  modalCountT += 1;
+});
