0f82d82372e923e58e0c598c79294c85abc635e6
ikoote1 <ikootepreim@gmail.com>
2023-02-01 16:09:50 +0300

    Added js code for the contact validation

--- index.js
+++ index.js
@@ -120,3 +120,19 @@ function closeModal(i) {
 }
 
 /* eslint-enable no-unused-vars */
+
+const form = document.querySelector('#acts');
+const errorSpan = document.querySelector('.error');
+
+const EMAIL_ERROR = 'Email should be in lower cases';
+
+function validateEmail(email) {
+  if (email.value === email.value.toLowerCase()) form.submit();
+  else errorSpan.innerHTML = EMAIL_ERROR;
+}
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  validateEmail(form.elements.nam);
+});
